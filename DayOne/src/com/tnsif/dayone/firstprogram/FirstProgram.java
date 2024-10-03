5d07143e0730ee6e54c21b608fe0241873f2d1c6
HP <HP@LAPTOP-REUQ2DKO>
2024-10-03 16:12:37 +0530

    Day one

---
+++ DayOne/src/com/tnsif/dayone/firstprogram/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayone.firstprogram;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	
+		System.out.println("..........Hello Everyone........... ");
+		System.out.println("----Welcome to TNSIF----");
+
+	}
+
+}
